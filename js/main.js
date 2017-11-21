ef862f3e3081bdebfc7e1aa4b82b77e2e51f43ab
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-21 23:18:11 +0000

    implements modal but login still not works

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ databaseSetUp();
 var database = firebase.database();
 var auth = firebase.auth();
 
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
+    });
+}
+
 retrieve('places', database).then(function(snapshot){
     places = snapshot.val();
     google.maps.event.addListenerOnce(map, 'idle', function(){
