882c8a564925a782162ce749809c25aa2980b1b7
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-22 22:49:58 +0000

    creates dashboard, separates mainjs in two

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ var auth = firebase.auth();
 function login() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    document.getElementById('email').value = ""; 
+    document.getElementById('email').value = ""
     auth.signInWithEmailAndPassword(email, password).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -18,8 +20,16 @@ function login() {
       console.log(errorCode);
       console.log(errorMessage);
       // ...
+    }).then(function() {
+      auth.onAuthStateChanged(function(user) {
+        if (user) {
+          window.location = 'dashboard.html';
+        }
+      });
     });
 }
+
+
 
 retrieve('places', database).then(function(snapshot){
     places = snapshot.val();
